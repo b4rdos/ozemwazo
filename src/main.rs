1edaaca30c1541d3fe2c6ec6389d5dfa4ad9f554
Konstantinos Anastasiou <con.anasta@gmail.com>
2025-03-19 20:07:00 +0000

    Add button and counter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| *set_count.write() += 1
+            class:red=move || count.get() % 2 == 1
+        >
+            <strong>"Click Me: "</strong>
+            {count}
+        </button>
+        <div class:container>
+            <p>
+                <strong>"Reactive: "</strong>
+                {move || count.get()}
+            </p>
+            <p>
+                <strong>"Reactive shorthand: "</strong>
+                {count}
+            </p>
+            <p>
+                <strong>"Not reactive: "</strong>
+                {count.get()}
+            </p>
+        </div>
+   }
+}
+
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello from leptos!"</p> } )
+    leptos::mount::mount_to_body(App)
 }
